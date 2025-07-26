c658d0e5ce15cea54b0568e501e9850b6228e961
Duminda Wanninayake <deemind@gmail.com>
2025-07-26 22:40:29 +0530

    Creating initial codebase with deps

---
+++ src/test/java/com/integrations/weather_stats/WeatherStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.integrations.weather_stats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
