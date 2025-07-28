e268fdd46c7cb821b8e7bee2e94e539673f1f910
Duminda Wanninayake <deemind@gmail.com>
2025-07-28 09:37:11 +0530

    Adding README + minor changes

--- src/test/java/com/integrations/weather/WeatherStatsServiceTests.java
+++ src/test/java/com/integrations/weather/WeatherStatsServiceTests.java
@@ -16,7 +16,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
-class WeatherStatsApplicationTests {
+class WeatherStatsServiceTests {
 
   @Mock
   private WeatherStatsService weatherStatsService;
